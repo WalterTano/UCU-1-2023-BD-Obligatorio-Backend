e33d4d65aa2b3a7d968234c0d8d2aa859af972b0
Santiago De Olivera <santiagodeolivera@gmail.com>
2023-06-11 16:58:09 -0300

    Add handle server error function

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,13 +1,27 @@
 import { RequestHandler } from "express";
 import { getUsers as getUsersModel, deleteUser as deleteUserModel } from "../models/user";
+import { Result } from "../types/result";
 
+function handleErrors<T>(handler: RequestHandler<T>): RequestHandler<T> {
+    return async (req, res, next) => {
+        try {
+            await handler(req, res, next);
+        } catch (e) {
+            console.error(e);
+            if (!res.writableEnded) {
+                const msg: Result<never> = { success: false, errorMsg: "Internal server error" };
+                res.status(500).json(msg);
+            }
+        }
+    }
+}
 
 export const getUsers: RequestHandler = async (req, res) => {
     const users = await getUsersModel();
     res.send(users);
 };
 
-export const getUser: RequestHandler<{userId: string}> = async (req, res) => {
+export const getUser: RequestHandler<{ userId: string }> = async (req, res) => {
     res.status(500).send("Not implemented yet");
 };
 
@@ -15,20 +29,22 @@ export const postUser: RequestHandler = async (req, res) => {
     res.status(500).send("Not implemented yet");
 };
 
-export const putUser: RequestHandler<{userId: string}> = async (req, res) => {
+export const putUser: RequestHandler<{ userId: string }> = async (req, res) => {
     res.status(500).send("Not implemented yet");
 };
 
-export const deleteUser: RequestHandler<{userId: string}> = async (req, res) => {
-    const userId = parseInt(req.params.userId);
-    if (isNaN(userId)) {
-        return res.status(400).send("Invalid CI");
-    }
+export const deleteUser: RequestHandler<{ userId: string }> = handleErrors(
+    async (req, res) => {
+        const userId = parseInt(req.params.userId);
+        if (isNaN(userId)) {
+            return res.status(400).send("Invalid CI");
+        }
 
-    const result = await deleteUserModel(userId);
-    if (result.success) {
-        res.status(200).json(result);
-    } else {
-        res.status(400).json(result);
+        const result = await deleteUserModel(userId);
+        if (result.success) {
+            res.status(200).json(result);
+        } else {
+            res.status(400).json(result);
+        }
     }
-};
+);
