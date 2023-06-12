5fcf3a506ae2ecc416bc1781b68f88c5b3d871bc
Santiago De Olivera <santiagodeolivera@gmail.com>
2023-06-11 22:23:37 -0300

    Add getSkillsOfUser function

---
+++ src/models/skill.ts
@@ -0,0 +1,17 @@
+import dbConn from "../configs/db.config";
+
+export async function getSkillsOfUser(ci: number): Promise<number[]> {
+    const sqlRes = await dbConn.select({
+        columns: ["id_hab"],
+        table: "usuario_tiene_habilidad",
+        conditions: [
+            { column: "ci", operation: "=", value: ci }
+        ]
+    });
+
+    if (!sqlRes.success) {
+        throw new Error(sqlRes.errorMessage);
+    }
+
+    return sqlRes.data.map(v => v.id_hab);
+}
