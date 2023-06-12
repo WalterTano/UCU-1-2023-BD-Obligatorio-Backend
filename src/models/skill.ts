1cb0210c71dc83281fb2a7b06c96cb4cf759a9f5
Santiago De Olivera <santiagodeolivera@gmail.com>
2023-06-11 22:29:14 -0300

    Add function getSkillNames

--- src/models/skill.ts
+++ src/models/skill.ts
@@ -15,3 +15,19 @@ export async function getSkillsOfUser(ci: number): Promise<number[]> {
 
     return sqlRes.data.map(v => v.id_hab);
 }
+
+export async function getSkillNames(skillIds: number[]): Promise<string[]> {
+    const sqlRes = await dbConn.select({
+        columns: ["nombre"],
+        table: "habilidad",
+        conditions: [
+            { column: "id", operation: "IN", value: skillIds }
+        ]
+    });
+
+    if (!sqlRes.success) {
+        throw new Error(sqlRes.errorMessage);
+    }
+
+    return sqlRes.data.map(v => v.nombre);
+}
