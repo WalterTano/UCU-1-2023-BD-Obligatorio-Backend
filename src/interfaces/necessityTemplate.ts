4f12ca59b0cb3e149ccecd05842e411e34a0815c
Santiago De Olivera <santiagodeolivera@gmail.com>
2023-06-13 17:37:39 -0300

    Start with POST necessity API

---
+++ src/interfaces/necessityTemplate.ts
@@ -0,0 +1,40 @@
+export interface DbNecessityTemplate {
+    id: number,
+    ci_creador: number,
+    descripcion: string,
+    estado: string,
+    lat_ubicacion: number,
+    long_ubicacion: number,
+    fecha_inicio: Date,
+    fecha_fin: Date | null,
+    fecha_solucionada: Date | null,
+    titulo: string
+}
+
+export interface NecessityTemplate {
+    necId: number,
+    userId: number,
+    description: string,
+    state: string,
+    latitude: number,
+    longitude: number,
+    startDate: Date,
+    endDate?: Date | null,
+    solvedDate?: Date | null,
+    title: string
+}
+
+export function necessityTemplateToDb(info: NecessityTemplate): DbNecessityTemplate {
+    return {
+        id: info.necId,
+        ci_creador: info.userId,
+        descripcion: info.description,
+        estado: info.state,
+        lat_ubicacion: info.latitude,
+        long_ubicacion: info.longitude,
+        fecha_inicio: info.startDate,
+        fecha_fin: info.endDate || null,
+        fecha_solucionada: info.solvedDate || null,
+        titulo: info.title
+    };
+}
---
+++ src/models/necessity.ts
@@ -0,0 +1,61 @@
+import dbConn from "../configs/db.config"
+import { SelectQuery } from "../db/interfaces/selectQuery";
+import { mapResult, unwrapResult } from "../helpers/resultHelpers";
+import { DbNecessity, Necessity, necessityFromDb } from "../interfaces/necessity"
+import { DbNecessityId } from "../interfaces/necessityId";
+import { NecessityTemplate, necessityTemplateToDb } from "../interfaces/necessityTemplate";
+import { Result } from "../types/result";
+
+// TODO: create endpoint for all necesities
+// TODO: create endpoint for all necesities with filters
+
+// TODO: do the same for other models
+async function selectAllFromNecessities(query: Omit<SelectQuery, "table" | "columns">): Promise<DbNecessity[]> {
+    const sqlRes = await dbConn.select({
+        columns: [
+            "id", "ci_creador", "descripcion", "estado", "lat_ubicacion",
+            "long_ubicacion", "fecha_inicio", "fecha_fin", "fecha_solucionada", "titulo"
+        ],
+        table: "necesidad",
+        ...query
+    });
+
+    const res: DbNecessity[] = unwrapResult(sqlRes);
+    return res;
+}
+
+export async function getNecessitiesByUser(userId: number): Promise<Necessity[]> {
+    const raw = await selectAllFromNecessities({
+        conditions: [
+            { column: "ci_creador", operation: "=", value: userId }
+        ]
+    });
+
+    return raw.map(necessityFromDb);
+}
+
+export async function getNecessityById(userId: number, necId: number): Promise<Necessity | undefined> {
+    const raw = await selectAllFromNecessities({
+        conditions: [
+            { column: "ci_creador", operation: "=", value: userId },
+            { column: "id", operation: "=", value: necId }
+        ]
+    });
+
+    const rawSingle = raw.at(0);
+    return rawSingle && necessityFromDb(rawSingle);
+}
+
+export async function insertNecessity(template: NecessityTemplate): Promise<Result<void>> {
+    const dbTemp = necessityTemplateToDb(template);
+
+    const sqlRes = await dbConn.insert({
+        table: "necesidad",
+        idColumns: ["id", "ci_creador"],
+        values: dbTemp
+    });
+
+    return mapResult(sqlRes, data0 => {
+        const data: DbNecessityId = data0;
+    });
+}
